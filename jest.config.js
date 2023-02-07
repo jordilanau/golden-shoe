5ea2b8b66eb19e76e520fdda2331c60b850a7e68
Jordi <jordi.lanau@and.digital>
2023-02-07 15:07:24 +0000

    Set test coverage to 70%

--- jest.config.js
+++ jest.config.js
@@ -13,10 +13,10 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.ts'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
   },
 };
