5f4aa6b4cecb19c5424bc292557a5e50eeb51b09
Jordi <jordi.lanau@and.digital>
2023-02-13 15:28:38 +0000

    Refactor layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,17 +1,17 @@
-import { FC, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import Header from '../header';
 
 type Props = {
   children?: ReactNode;
 };
 
-const Layout: FC<Props> = ({ children }) => {
+function Layout({ children }: Props) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-200" data-testid="layout-container">
       <Header />
       {children}
     </div>
   );
-};
+}
 
 export default Layout;
