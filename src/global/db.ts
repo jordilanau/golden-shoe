ec37f8cf4497483c672f70205eaade0e180259d8
Jordi <jordi.lanau@and.digital>
2023-02-23 09:28:12 +0000

    Initialise prisma

---
+++ src/global/db.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
