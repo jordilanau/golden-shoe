a5e8afa8658cc3c1a66813a5bd343f5aabc10bff
Jordi <jordi.lanau@and.digital>
2023-02-24 11:26:33 +0000

    Add ShoeVariant model and Shoe seeds

---
+++ prisma/seeds.ts
@@ -0,0 +1,125 @@
+import { faker } from '@faker-js/faker';
+import { Gender } from '@prisma/client';
+import { prisma } from '../src/global/db';
+import {
+  IMG_PLACEHOLDER,
+  MEN_BOOTS,
+  MEN_LOAFERS,
+  MEN_SANDALS,
+  MEN_SHOES,
+  MEN_SNEAKERS,
+  WOMEN_BOOTS,
+  WOMEN_HEELS,
+  WOMEN_LOAFERS,
+  WOMEN_SANDALS,
+  WOMEN_SNEAKERS,
+} from '../src/utils/shoeImages';
+
+async function main() {
+  await prisma.shoe.deleteMany({});
+
+  async function createShoePair() {
+    const gender: Gender = faker.helpers.arrayElement([Gender.Men, Gender.Women]);
+    const category =
+      gender === Gender.Men
+        ? faker.helpers.arrayElement(['MEN_SNEAKERS', 'MEN_LOAFERS', 'MEN_BOOTS', 'MEN_SHOES', 'MEN_SANDALS'])
+        : faker.helpers.arrayElement([
+            'WOMEN_SNEAKERS',
+            'WOMEN_BOOTS',
+            'WOMEN_HEELS',
+            'WOMEN_LOAFERS',
+            'WOMEN_SANDALS',
+          ]);
+    let image: string;
+    switch (category) {
+      case 'MEN_SNEAKERS': {
+        image = MEN_SNEAKERS;
+        break;
+      }
+      case 'MEN_LOAFERS': {
+        image = MEN_LOAFERS;
+        break;
+      }
+      case 'MEN_BOOTS': {
+        image = MEN_BOOTS;
+        break;
+      }
+      case 'MEN_SHOES': {
+        image = MEN_SHOES;
+        break;
+      }
+      case 'MEN_SANDALS': {
+        image = MEN_SANDALS;
+        break;
+      }
+      case 'WOMEN_SNEAKERS': {
+        image = WOMEN_SNEAKERS;
+        break;
+      }
+      case 'WOMEN_BOOTS': {
+        image = WOMEN_BOOTS;
+        break;
+      }
+      case 'WOMEN_HEELS': {
+        image = WOMEN_HEELS;
+        break;
+      }
+      case 'WOMEN_LOAFERS': {
+        image = WOMEN_LOAFERS;
+        break;
+      }
+      case 'WOMEN_SANDALS': {
+        image = WOMEN_SANDALS;
+        break;
+      }
+      default: {
+        image = IMG_PLACEHOLDER;
+        break;
+      }
+    }
+
+    await prisma.shoe.create({
+      data: {
+        sku: faker.random.numeric(10),
+        model: 'shoe-' + faker.random.alphaNumeric(5),
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta nulla mauris.',
+        gender,
+        category,
+        image,
+        size: '8',
+        stock: parseInt(faker.random.numeric(1)),
+        variants: {
+          create: [
+            {
+              sku: faker.random.numeric(10),
+              size: '9',
+              stock: parseInt(faker.random.numeric(1)),
+            },
+            {
+              sku: faker.random.numeric(10),
+              size: '10',
+              stock: parseInt(faker.random.numeric(1)),
+            },
+            {
+              sku: faker.random.numeric(10),
+              size: '11',
+              stock: parseInt(faker.random.numeric(1)),
+            },
+          ],
+        },
+      },
+    });
+  }
+
+  for (let i = 0; i < 50; i++) {
+    await createShoePair();
+  }
+}
+
+main()
+  .then(async () => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ src/utils/shoeImages.ts
@@ -0,0 +1,23 @@
+export const MEN_SNEAKERS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/sneakers_too9nm.jpg';
+export const MEN_LOAFERS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067753/golden-shoe/men-loafers_d4pw8w.jpg';
+export const MEN_BOOTS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/v1677067752/golden-shoe/men-boots_jsdta8.jpg';
+export const MEN_SHOES =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/v1677067752/golden-shoe/men-shoes_go87v9.jpg';
+export const MEN_SANDALS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/v1677067751/golden-shoe/men-sandals_ptq70a.jpg';
+
+export const WOMEN_SNEAKERS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/sneakers_too9nm.jpg';
+export const WOMEN_BOOTS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/women-boots_kxfnkr.jpg';
+export const WOMEN_HEELS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/women-heels_lzbqe2.jpg';
+export const WOMEN_SANDALS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067752/golden-shoe/women-sandals_hnbb7o.jpg';
+export const WOMEN_LOAFERS =
+  'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/women-loafers_hln13a.jpg';
+
+export const IMG_PLACEHOLDER = 'https://via.placeholder.com/150';
