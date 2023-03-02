5234dbe210e7a36b6e1fba7412002a16209befb5
Jordi <jordi.lanau@and.digital>
2023-03-02 09:19:18 +0000

    Add queries and mutations for Category

--- src/graphql/types/Category.ts
+++ src/graphql/types/Category.ts
@@ -1,3 +1,4 @@
+import { prisma } from '../../global/db';
 import { builder } from '../builder';
 
 builder.prismaObject('Category', {
@@ -29,3 +30,45 @@ export const CategoryValues = builder.prismaObject('Category', {
     category: t.exposeString('category'),
   }),
 });
+
+builder.queryField('getAllCategories', (t) =>
+  t.prismaField({
+    type: [CategoryValues],
+    resolve: (query, _parent, _args, _ctx, _info) => prisma.category.findMany({ ...query, include: { _count: true } }),
+  })
+);
+
+builder.queryField('getCategoryById', (t) =>
+  t.prismaField({
+    type: 'Category',
+    args: {
+      id: t.arg.int({ required: true }),
+    },
+    nullable: true,
+    resolve: (query, _parent, args, _ctx, _info) => prisma.category.findUnique({ ...query, where: { id: args.id } }),
+  })
+);
+
+builder.queryField('getCategoryByName', (t) =>
+  t.prismaField({
+    type: 'Category',
+    args: {
+      category: t.arg.string({ required: true }),
+    },
+    nullable: true,
+    resolve: async (query, _parent, args, _ctx, _info) =>
+      prisma.category.findUnique({ ...query, where: { category: args.category } }),
+  })
+);
+
+builder.mutationField('createCategory', (t) =>
+  t.prismaField({
+    type: CategoryValues,
+    nullable: true,
+    args: {
+      category: t.arg.string({ required: true }),
+    },
+    resolve: async (mutation, _parent, args, _ctx, _info) =>
+      await prisma.category.create({ data: { category: args.category } }),
+  })
+);
