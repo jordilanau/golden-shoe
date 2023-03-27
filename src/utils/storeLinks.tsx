f574fa0685d155a3326a5c9ba66b8eb470ffbf7f
Jordi <jordi.lanau@and.digital>
2023-03-27 17:28:04 +0100

    Add images to storeLinks

--- src/utils/storeLinks.tsx
+++ src/utils/storeLinks.tsx
@@ -1,7 +1,7 @@
 export type StoreLinkType = {
   text: string;
   href: string;
-  categories: { text: string; href: string }[];
+  categories: { text: string; href: string; image: string }[];
 };
 
 export type StoreProps = {
@@ -16,48 +16,68 @@ export const storeLinks: StoreLinkType[] = [
       {
         text: 'trainers',
         href: '/men/trainers',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/sneakers_too9nm.jpg',
       },
       {
         text: 'boots',
         href: '/men/boots',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067752/golden-shoe/men-boots_jsdta8.jpg',
       },
       {
         text: 'shoes',
         href: '/men/shoes',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067752/golden-shoe/men-shoes_go87v9.jpg',
       },
       {
         text: 'loafers',
         href: '/men/loafers',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067753/golden-shoe/men-loafers_d4pw8w.jpg',
       },
       {
         text: 'sandals',
         href: '/men/sandals',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/men-sandals_ptq70a.jpg',
       },
     ],
   },
   {
     text: 'women',
-    href: '/men',
+    href: '/women',
     categories: [
       {
         text: 'trainers',
-        href: '/men/trainers',
+        href: '/women/trainers',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/sneakers_too9nm.jpg',
       },
       {
         text: 'boots',
-        href: '/men/boots',
+        href: '/women/boots',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/women-boots_kxfnkr.jpg',
       },
       {
         text: 'heels',
-        href: '/men/heels',
+        href: '/women/heels',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/women-heels_lzbqe2.jpg',
       },
       {
         text: 'loafers',
-        href: '/men/loafers',
+        href: '/women/loafers',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067751/golden-shoe/women-loafers_hln13a.jpg',
       },
       {
         text: 'sandals',
-        href: '/men/sandals',
+        href: '/women/sandals',
+        image:
+          'https://res.cloudinary.com/drmt1q1rs/image/upload/q_auto:good/v1677067752/golden-shoe/women-sandals_hnbb7o.jpg',
       },
     ],
   },
